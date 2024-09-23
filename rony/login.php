7708616f4da46de0eb2aeefa21856699c152a5c8
RonnyeZ <121881238+RonnyeZ@users.noreply.github.com>
2024-09-23 11:00:46 -0300

    Add files via upload

--- rony/login.php
+++ rony/login.php
@@ -11,11 +11,14 @@ $senhacripto = cripto($senha);
 $select = "SELECT login, senha, nome, nivel 
 FROM usuario WHERE login = '$login' AND senha = '$senhacripto'";
 
-if ($login == "admin" && $senha == "admin") {
+$query = mysqli_query($con, $select);
+$dados = mysqli_fetch_row($query);
+
+if ($login == isset($dados[0]) && $senha == isset($dados[1])) {
     session_start();
     $_SESSION["logado"] = true;
-    $_SESSION["nome"] = "Administrador";
-    $_SESSION["nivel"] = 1;
+    $_SESSION["nome"] = $dados[2];
+    $_SESSION["nivel"] = $dados[3];
     header("Location: principal.php");
 } else {
     echo '<script>alert("Usuario ou senha incorretas");
